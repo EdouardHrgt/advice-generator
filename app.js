d3cbd50b26dff3bc2474e992e1287a167076f728
Edouard Herrengt <86005588+EdouardHrgt@users.noreply.github.com>
2022-08-27 17:37:34 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector('.logo');
+btn.addEventListener('click', () => {
+  quoteGenerator();
+});
+
+function quoteGenerator() {
+  fetch('https://api.adviceslip.com/advice')
+    .then((res) => {
+      return res.json();
+    })
+    .then((dataList) => {
+      const quote = dataList.slip;
+      console.log(quote.advice);
+      createEl(quote.advice);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+function createEl(text) {
+  const container = document.querySelector('.quote__wrapper');
+
+  if (container.firstChild) {
+    container.removeChild(container.firstElementChild);
+  }
+  
+  const blockQuote = document.createElement('blockquote');
+  const newQuote = document.createTextNode(text);
+
+  blockQuote.appendChild(newQuote);
+  container.appendChild(blockQuote);
+}
